9733cd54a708f3fe663c7f46ca816ef116250676
Jyotirmoy Roy <jroy2516@uni.sydney.edu.au>
2017-10-19 14:20:45 +1100

    firebase users now display in the listview

--- app/src/main/java/comp5216/au/edu/uni/usyd/admincomp5216/ChatDeployActivity.java
+++ app/src/main/java/comp5216/au/edu/uni/usyd/admincomp5216/ChatDeployActivity.java
@@ -2,8 +2,15 @@ package comp5216.au.edu.uni.usyd.admincomp5216;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ListView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,20 +20,45 @@ public class ChatDeployActivity extends AppCompatActivity {
     ListView mUsersListView;
     UserAdapter mUserAdapter;
 
+    FirebaseDatabase mFirebaseDatabase;
+    DatabaseReference mUserDatabaseReference;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_deploy);
 
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+
+
         mUsersListView = (ListView) findViewById(R.id.listview);
 
-        List<User> usersList = new ArrayList<>();
-        usersList.add(new User("Jay", "image", "status", "thumb_image"));
-
-
+        final List<User> usersList = new ArrayList<>();
         mUserAdapter = new UserAdapter(this, R.layout.item_user, usersList);
         mUsersListView.setAdapter(mUserAdapter);
 
+        mUserDatabaseReference = mFirebaseDatabase.getReference().child("Users");
+        mUserDatabaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for(DataSnapshot user : dataSnapshot.getChildren()) {
+                    User userObject = user.getValue(User.class);
+                    usersList.add(userObject);
+                    mUserAdapter.notifyDataSetChanged();
+                    Log.d("User", userObject.getName());
+
+                }
+
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
+
+
+        
 
     }
 }
