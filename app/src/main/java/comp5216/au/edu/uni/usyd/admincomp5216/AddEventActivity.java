19bce9842a66ef4c427a245dc1c2321cb9ea3ba0
Jyotirmoy Roy <jroy2516@uni.sydney.edu.au>
2017-10-23 02:06:01 +1100

    new events are saved to firebase

--- app/src/main/java/comp5216/au/edu/uni/usyd/admincomp5216/AddEventActivity.java
+++ app/src/main/java/comp5216/au/edu/uni/usyd/admincomp5216/AddEventActivity.java
@@ -2,12 +2,56 @@ package comp5216.au.edu.uni.usyd.admincomp5216;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class AddEventActivity extends AppCompatActivity {
+
+    EditText mEventName;
+    EditText mEventLocation;
+    EditText mEventType;
+    EditText mEventDesc;
+
+    Button mSubmitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_event);
+
+        mEventName = (EditText) findViewById(R.id.editTextEventName);
+        mEventLocation = (EditText) findViewById(R.id.editTextEventLocation);
+        mEventType = (EditText) findViewById(R.id.editTextEventType);
+        mEventDesc = (EditText) findViewById(R.id.editTextEventDesc);
+
+        mSubmitButton = (Button) findViewById(R.id.btnSubmit);
+
+        mSubmitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String title = mEventName.getText().toString();
+                String locaiton = mEventLocation.getText().toString();
+                String desc = mEventDesc.getText().toString();
+                String type = mEventType.getText().toString();
+
+                if(title != null && !title.isEmpty()){
+                    Event event = new Event(title, locaiton, type, desc);
+                    FirebaseDatabase.getInstance().getReference().child("Events").setValue(event);
+                } else{
+                    Toast.makeText(AddEventActivity.this, "You need to give a title", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+
+
+
     }
 }
